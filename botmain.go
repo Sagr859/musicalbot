8d5fe426e0ec98aa5548c6996277d3913ca32547
Sagar Sajeev <sagaramss@gmail.com>
2020-05-06 17:33:12 +0530

    Add files via upload

--- botmain.go
+++ botmain.go
@@ -3,16 +3,38 @@ package main
 import (
 	"log"
 	"strings"
-	"github.com/go-telegram-bot-api/telegram-bot-api"
+	tb "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/rhnvrm/lyric-api-go"
+	"os"
+	"encoding/json"
+    "io"
+    "io/ioutil"
+   "github.com/gin-gonic/gin"
+   _ "github.com/heroku/x/hmetrics/onload"
+	_ "github.com/lib/pq"
+	"gofmt"
 )
+var (
+	bot       *tgbotapi.BotAPI
+	baseURL = "bitovenbot.herokuapp.com" 
+	token     = os.Getenv("1046861512:AAGOnyKtCMcRVebYosmBNO9ZUQbwDVyGcsU")      
+)
+func initTelegram(){
+	var err error
 
-func main() {
-	bot, err := tgbotapi.NewBotAPI("1046861512:AAGOnyKtCMcRVebYosmBNO9ZUQbwDVyGcsU")
-	if err != nil {
-		log.Panic(err)
-	}
+    bot, err = tgbotapi.NewBotAPI(token)
+    if err != nil {
+        log.Println(err)
+        return
+    }
 
+    // this perhaps should be conditional on GetWebhookInfo()
+    // only set webhook if it is not set properly
+    url := baseURL + bot.Token
+    _, err = bot.SetWebhook(tgbotapi.NewWebhook(url))
+    if err != nil {
+        log.Println(err)
+	} 
 	bot.Debug = true
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
@@ -61,6 +83,51 @@ func main() {
 		}
 
 	}
+
+}
+
+func webhookHandler(c *gin.Context){
+	defer c.Request.Body.Close()
+
+    bytes, err := ioutil.ReadAll(c.Request.Body)
+    if err != nil {
+        log.Println(err)
+        return
+    }
+
+    var update tgbotapi.Update
+    err = json.Unmarshal(bytes, &update)
+    if err != nil {
+        log.Println(err)
+        return
+    }
+
+    // to monitor changes run: heroku logs --tail
+    log.Printf("From: %+v Text: %+v\n", update.Message.From, update.Message.Text)
+
+}
+func main() {
+
+    port := os.Getenv("PORT")
+
+    if port == "" {
+        log.Fatal("$PORT must be set")
+    }
+
+    // gin router
+    router := gin.New()
+    router.Use(gin.Logger())
+
+    // telegram
+    initTelegram()
+    router.POST("/" + bot.Token, webhookHandler)
+
+    err := router.Run(":" + port)
+    if err != nil {
+        log.Println(err)
+	}
+	
+	
 }
 
 func GetLyrics(artist string, song string) string {
